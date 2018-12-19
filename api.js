a8eff3586fedbb00c46a332083d1ea6b597eb921
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-19 13:04:18 +0100

    Inseriti test

--- api.js
+++ api.js
@@ -1,6 +1,7 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const fetch = require("node-fetch")
+const https = require("http")
 
 const app = express()
 app.use( bodyParser.json() )
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000
 
-const BASE_URL = "https://tomaiuolo-186410-b-dec2018.herokuapp.com";
+const BASE_URL = "http://localhost:3000";
 
 
 app.get('/', (req, res) => {
@@ -22,21 +23,31 @@ app.get("/bimbumbam", (req,res) => {
 })
 
 app.get("/play", (req, res) => {
-    let player = await fetch (BASE_URL+"/bimbumbam");
+    const p2 = Math.ceil(Math.random()*5) ;
+    if(req && req.query && req.query.player1){
+        
+        if(parseInt(req.query.player1)<1 || parseInt(req.query.player1)>5){
+            console.log(req.query.player1)
+            return res.status(400).json();
+        }
+        else {
+            let sum  = parseInt(p2) + parseInt(req.query.player1);
+            console.log(sum)
+            let pari = 1;
 
-    let sum  = player + req.query.player;
-    let pari = 1;
+            if (sum %2 == 0) {
+                pari = 0;
+            }
 
-    if (sum %2 == 0) {
-        pari = 0;
+            let response = {
+                result: pari,
+                player2: p2
+            }
+
+            return res.status(200).json (response);
+        }
     }
-
-    let response = {
-        result: pari,
-        player2: player
-    }
-
-    res.json (response);
+    return res.status(400).json();
 })
 
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
