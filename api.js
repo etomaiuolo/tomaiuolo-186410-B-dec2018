c2d7d46161ec9a6802fd4a830941c20fd0930435
Elena Tomaiuolo <elena.tomaiuolo@live.it>
2018-12-19 11:52:18 +0100

    Aggiunto endpoint /play

--- api.js
+++ api.js
@@ -9,6 +9,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000
 
+const BASE_URL = "https://tomaiuolo-186410-b-dec2018.herokuapp.com";
+
 
 app.get('/', (req, res) => {
     res.json({status: 'ok'})
@@ -17,6 +19,24 @@ app.get('/', (req, res) => {
 app.get("/bimbumbam", (req,res) => {
     const number = Math.ceil(Math.random()*5);
     res.json({result: number});
+})
+
+app.get("/play", (req, res) => {
+    let player = await fetch (BASE_URL+"/bimbumbam");
+
+    let sum  = player + req.query.player;
+    let pari = 1;
+
+    if (sum %2 == 0) {
+        pari = 0;
+    }
+
+    let response = {
+        result: pari,
+        player2: player
+    }
+
+    res.json (response);
 })
 
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
